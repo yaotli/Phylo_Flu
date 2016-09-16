dc57ab4c6ee074aa4cb89678b8fa1324f866730d
Yao Li <nprotei@gmail.com>
2016-09-17 02:55:13 +0800

    gene duplication & subtree

--- _R/Functions.R
+++ _R/Functions.R
@@ -13,3 +13,144 @@ getDescendants<-function(tree,node,curr=NULL){
     curr<-curr[which(curr <= ll)]
   
   return(curr) }
+
+
+##### 
+
+########## Arrange duplicated name ########## 
+
+
+rmdup<-function(file){
+  
+  library(seqinr) 
+  
+  file = read.fasta(file.choose())
+  
+  seq.name0 = attributes(file)$names
+  seq0 = getSequence(file)
+  
+  duplicated.id = which(duplicated(seq.name0) == "TRUE")
+  
+  ls.name0 = as.list(seq.name0[duplicated.id])
+  ls.name = sapply(ls.name0, function(x){ paste0(x[1], "b")   })
+  
+  seq.name0[duplicated.id] = ls.name
+  
+  write.fasta(seq0, file.out = "noduplicated.fasta", names = seq.name0)
+  
+  print("Done")
+  
+}
+
+########## Subtree seq extraction ########## 
+
+
+subtreseq<-function(){
+  
+  library(seqinr) 
+  
+  fasta0 = read.fasta(file.choose())
+  seq.name0 = attributes(fasta0)$names
+  seq0 = getSequence(fasta0)
+  bug.id = grep("'", seq.name0)
+  
+  sub.tree=read.csv(file.choose())
+  
+  tipslabel.subtree0 = as.character(sub.tree[,1])
+  startno = which(tipslabel.subtree0 == "\ttaxlabels") + 1
+  endno = head(which(tipslabel.subtree0 == ";"), 1) -1
+  
+  tipslabel.subtree = tipslabel.subtree0[startno:endno]
+  
+  name.subtree = sapply(strsplit(tipslabel.subtree, split = "'", fixed = T), function(x)x[2])
+  id.subtree= match(name.subtree, seq.name0)
+  
+  if( any(NA %in% id.subtree) == "TRUE"){
+    
+    bug = which(is.na(id.subtree))
+    checkpoint = length(grep(name.subtree[bug], seq.name0[bug.id]))
+    
+    if (  (checkpoint >= 1) & (checkpoint == length(bug) ) ){
+      
+      id.subtree[bug] = bug.id[grep(name.subtree[bug], seq.name0[bug.id])]
+      
+      
+      seq.name0.subtree = seq.name0[id.subtree]
+      seq0.subtree = seq0[id.subtree]
+      
+      write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
+      
+      print("Done")
+      
+      
+    }else(
+      print(name.subtree[which(id.subtree %in% NA)]))  } else{
+        
+        seq.name0.subtree = seq.name0[id.subtree]
+        seq0.subtree = seq0[id.subtree]
+        
+        write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
+        
+        print("Done")
+        
+      } }
+
+subtreseq2<-function(){
+  
+  library(seqinr) 
+  
+  fasta0 = read.fasta(file.choose())
+  seq.name0 = attributes(fasta0)$names
+  seq0 = getSequence(fasta0)
+  bug.id = grep("'", seq.name0)
+  
+  sub.tree=read.csv(file.choose())
+  
+  tipslabel.subtree0 = as.character(sub.tree[,1])
+  startno = which(tipslabel.subtree0 == "\ttaxlabels") + 1
+  endno = head(which(tipslabel.subtree0 == ";"), 1) -1
+  
+  tipslabel.subtree = tipslabel.subtree0[startno:endno]
+  
+  name.subtree = sapply(strsplit(tipslabel.subtree, split = "'", fixed = T), function(x)x[2])
+  id.subtree= match(name.subtree, seq.name0)
+  
+  if( any(NA %in% id.subtree) == "TRUE"){
+    
+    bug = which(is.na(id.subtree))
+    checkpoint = length(grep(name.subtree[bug], seq.name0[bug.id]))
+    
+    if (  (checkpoint >= 1) & (checkpoint == length(bug) ) ){
+      
+      id.subtree[bug] = bug.id[grep(name.subtree[bug], seq.name0[bug.id])]
+      
+      
+      seq.name0.subtree = seq.name0[id.subtree]
+      seq0.subtree = seq0[id.subtree]
+      
+      seq.name2.subtree = seq.name0[-id.subtree]
+      seq2.subtree = seq0[-id.subtree]
+      
+      write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
+      write.fasta(seq2.subtree, file.out = "remain.fasta", names = seq.name2.subtree)
+      
+      
+      print("Done")
+      
+      
+    }else(
+      print(name.subtree[which(id.subtree %in% NA)]))  } else{
+        
+        seq.name0.subtree = seq.name0[id.subtree]
+        seq0.subtree = seq0[id.subtree]
+        
+        seq.name2.subtree = seq.name0[-id.subtree]
+        seq2.subtree = seq0[-id.subtree]
+        
+        write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
+        write.fasta(seq2.subtree, file.out = "remain.fasta", names = seq.name2.subtree)
+        
+        
+        print("Done")
+        
+      } }
