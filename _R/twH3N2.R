0a656cd7fe900af89271b4c3269896743373d27e
Yao Li <nprotei@gmail.com>
2016-12-26 22:30:31 +0800

    a major revise: ID clean, curate and dist of TW data

--- _R/twH3N2.R
+++ _R/twH3N2.R
@@ -2,60 +2,133 @@ library(seqinr)
 
 # ID cleaning ####
 
-   gisaidh3 <- cleanID()
-pooledh3_tw <- cleanID()
+  g_gb_pooled_22685 <- cleanID()
+       g_gb_fg_4863 <- cleanID()
+      tw_pooled_168 <- cleanID()
 
 
 # seq curation ####
 
 # maxamb = 5, minseq = 1500, replicates are allowed in TW data
-# 1647 / 15 reps
 
-    gisaidh3_cr <- curateSeq(5, 1500, 1)
- pooledh3_tw_cr <- curateSeq(5, 1500, 0)
- 
- 
+    # 39; 1647; 3180
+        g_gb_fg_cleanID <- curateSeq(5, 1500, 1)
+    
+    # 58; 7479; 15151
+    g_gb_pooled_cleanID <- curateSeq(5, 1500, 1)
+    
+    # 0; 15; 157
+      tw_pooled_cleanID <- curateSeq(5, 1500, 0)
+    
+    # 0; 129; 15186
+            pooled_gbtw <- curateSeq(5, 1500, 0)
+    
+    # 0; 14; 3331      
+         pooled_gbtw_fg <- curateSeq(5, 1500, 0)
+      
+      
 # align and trim
-# remove apparent duplicated data (Taiwan)
  
-library(seqinr)
+# Distribution of TW data ####
+ 
+library(ggplot2)
+library(ggtree)
+library(ape)
 library(stringr)
  
- # read in 
- file = read.fasta(file.choose())
+ # read-in
  
- seq_name0 = attributes(file)$names
-      seq0 = getSequence(file)
+ s3273_pooled_gbtw_fg_align <- read.tree(file.choose())
+ ggtree(s3273_pooled_gbtw_fg_align)
  
-   toberemove <- pooledh3_tw_cr[-(8:11)]
- toberemoveid <-match(toberemove, seq_name0)
+ # tablized tree data
  
- ramain <- seq(1:length(seq0))[-toberemoveid]
+ treedata_s3273_pooled_gbtw_fg <- fortify(s3273_pooled_gbtw_fg_align)   
  
- seq_name_out = seq_name0[ramain]
-      seq_out = seq0[ramain] 
+ # _ID
  
- write.fasta(seq_out, 
-             file.out = "~/Desktop/out_seq.fasta", 
-             names = seq_name_out)         
+ treedata_s3273_id <- gsub("'", "",
+                           treedata_s3273_pooled_gbtw_fg$label[which(treedata_s3273_pooled_gbtw_fg$label != "NA")])
+ # _time
  
+ d = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+ treedata_s3273_date <- str_match(treedata_s3273_id, d)[,1]
  
- # deal with replated between TW from gisaid pool
- # since ERROR from FastTree, remove additional 6 seqs
- # resulting file: allpooled_trim_mo2
+ # _isolation place
  
- tonoredundant()
+ s = "A/([A-Za-z0-9-_]+)/"
+ treedata_s3273_place <- str_match(treedata_s3273_id, s)[,2]
  
+ # label TW isolates
+ # number = 150
  
-# 
+ treedata_s3273_TW <- rep(0, length(treedata_s3273_place))
+ treedata_s3273_TW[grep("Tai",treedata_s3273_place)] <- 1
+ treedata_s3273_id[which(treedata_s3273_TW == 1)]
  
+ du = "/([0-9]{4})_([a-z])_([0-9{4}])"
  
+ # eliminate seq with same info (ex: _a_)
+ # resulting TW isolates: 128
  
+ treedata_s3273_TWb = rep(0, length(treedata_s3273_place))
+ treedata_s3273_TWb[ which(treedata_s3273_TW == 1)[-grep(du, treedata_s3273_id[which(treedata_s3273_TW == 1)])] ] <- 1
  
+   s3273_TW_id <- treedata_s3273_id[which(treedata_s3273_TWb == 1)]
+ s3273_TW_date <- str_match(s3273_TW_id, d)[,1]
  
+ # s3273_TW_id[117] without month and date info
+ # which(is.na(phylo_date(s3273_TW_date)) == TRUE)
  
+ # continuous varibale of date
+ s3273_TW_time <- phylo_date(s3273_TW_date)
  
+ # deal with TW CDC chart
+ # http://nidss.cdc.gov.tw/en/SingleDisease.aspx?dc=1&dt=4&disease=487a
+ twcdcchart <- read.csv(file.choose())[,c(1,4)]
+ colnames(twcdcchart) <- c("Month", "Case")
  
- 
+ # epi-curve
+   
+   llb <- rep(2009: 2016, each = 2 )
+   llb[seq(1,15,2)] = paste0(llb[seq(1,15,2)], "/1")
+   llb[seq(2,16,2)] = paste0(llb[seq(2,16,2)], "/7")
+   llb <- gsub("20", "", llb)
+   
+   p<-ggplot(twcdcchart, aes(x = c(1:96), y = Case)) + 
+   geom_point(size = 1.5) + 
+   geom_line(size = 1 ) +
+   theme_bw() + 
+   scale_x_continuous(breaks = c(seq(1, 96, by = 6)), 
+                      labels = llb) + 
+   xlab("") 
+
+ # make sample distribution 
+ # only 115 seqs during 20090101 - 20161230 are available
+   
+   s3273_TW_time_dist <- epi_tomonth(200901, 201612, 
+                                     s3273_TW_date[-117][which(s3273_TW_date[-117] > "2009")])
+   
+   q<-ggplot(s3273_TW_time_dist, aes(x = c(1:96), y = Freq)) + 
+     geom_line(size = 1, color = "Blue") +
+     theme_bw() + 
+     scale_x_continuous(breaks = c(seq(1, 96, by = 6)), 
+                        labels = c()) + 
+     xlab("") + 
+     ylab("available Seq")
+   
+  # combine 2 fig
+  multiplot(q, p, ncol =1) 
+   
+   
+# locate TW strains on the tree ####
+  
+  
+  
+  
+  
+  
+  
+   
  
  
