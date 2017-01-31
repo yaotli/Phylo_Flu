b4d51da0083193e9c4fdea18ee725e1fbc630815
Yao Li <nprotei@gmail.com>
2017-01-31 10:00:52 +0800

    simplified subtreseq, leave subtreseq2

--- _R/Functions.R
+++ _R/Functions.R
@@ -150,53 +150,24 @@ subtreseq<-function(){
   fasta0 = read.fasta(file.choose())
   seq.name0 = attributes(fasta0)$names
   seq0 = getSequence(fasta0)
-  bug.id = grep("'", seq.name0)
   
-  sub.tree=read.csv(file.choose())
-  
-  tipslabel.subtree0 = as.character(sub.tree[,1])
-  startno = which( tipslabel.subtree0 == "\ttaxlabels" ) + 1
-  endno = head( which(tipslabel.subtree0 == ";"), 1 ) - 1
-  
-  tipslabel.subtree = tipslabel.subtree0[ startno:endno ]
-  
-  name.subtree = sapply(strsplit(tipslabel.subtree, split = "'", fixed = T), function(x)x[2])
-  id.subtree = match(name.subtree, seq.name0)
+  sub.tree = read.csv(file.choose(), header = FALSE, stringsAsFactors = FALSE)
+  id.subtree = match(sub.tree[,1], seq.name0)
   
   if( any(NA %in% id.subtree) == "TRUE"){
     
-    bug = which(is.na(id.subtree))
+    print("Mismatch")
     
-    match.id = c()
-    for(i in 1: length(bug)){
-      match = grep(name.subtree[bug][i], seq.name0[bug.id])
-      if ( length(match) == 1 ){ match.id[length(match.id) + 1] = match} }
+  }else{
     
-    if (  ( length(match.id) >= 1) & ( length(match.id) == length(bug) ) ){
-      
-      id.subtree[bug] = bug.id[match.id]
-      
-      
-      seq.name0.subtree = seq.name0[id.subtree]
-      seq0.subtree = seq0[id.subtree]
-      
-      write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
-      
-      print("Done")
-      
-      
-    }else{
-      problem = c(bug, name.subtree[bug])
-      print( problem ) }  } else{
-        
-        seq.name0.subtree = seq.name0[id.subtree]
-        seq0.subtree = seq0[id.subtree]
-        
-        write.fasta(seq0.subtree, file.out = "subtree.fasta", names = seq.name0.subtree)
-        
-        print("Done")
-        
-      } }
+    seq.name0_subtree = seq.name0[id.subtree]
+    seq0_subtree = seq0[id.subtree]
+    
+    write.fasta(seq0_subtree, file.out = "~/Desktop/subtree.fasta", names = seq.name0_subtree)
+    
+    print("Done")
+    
+  } }
 
 subtreseq2<-function(){
   
